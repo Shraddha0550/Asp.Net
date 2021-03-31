498a652e9cce053b3d97452dba60a2f8087b032e
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-03-31 15:08:40 -0400

    Rating Managment System

---
+++ Rating_Mgt_System/Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="Rating">
+    <div class ="Head">
+    <h1> Rating System </h1>
+    
+    </div>
+    <div class ="Body">
+     
+        <asp:ImageButton ID="img1" runat="server" Height="27px" 
+            ImageUrl="~/img/Star.gif" onclick="ImageButton1_Click" 
+            Width="24px" />
+        <asp:ImageButton ID="ImageButton1" runat="server" Enabled="False" Height="27px" 
+            ImageUrl="~/img/Star.gif" onclick="ImageButton1_Click1" Width="24px" />
+        <asp:ImageButton ID="ImageButton2" runat="server" Enabled="False" Height="27px" 
+            ImageUrl="~/img/Star.gif" onclick="ImageButton2_Click" Width="24px" />
+        <asp:ImageButton ID="ImageButton3" runat="server" Enabled="False" Height="27px" 
+            ImageUrl="~/img/Star.gif" onclick="ImageButton3_Click" Width="24px" />
+        <asp:ImageButton ID="ImageButton4" runat="server" Enabled="False" Height="27px" 
+            ImageUrl="~/img/Star.gif" onclick="ImageButton4_Click" Width="24px" />
+            
+       &nbsp &nbsp &nbsp &nbsp &nbsp
+        <asp:Button ID="Button1" runat="server" Text="Submit" Width="102px" 
+           Height="31px" onclick="Button1_Click" />
+
+           <br />
+           <br />
+        <asp:Label ID="Label1" runat="server" Enabled="False"></asp:Label>
+
+            
+       
+        <br />
+     </div>
+
+         </div>
+       
+       
+       
+   
+    </form>
+</body>
+</html>
---
+++ Rating_Mgt_System/Default.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    static int val1 = 0;
+    static int val2 = 0;
+    static int val3 = 0;
+    static int val4 = 0;
+    static int val5 = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Enabled = false;  
+    }
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+        
+        if (val1 == 0)
+        {
+            img1.ImageUrl = "~/img/FilledStar.gif";
+            ImageButton1.Enabled = true;
+            val1++;
+        }
+        else
+        {
+            img1.ImageUrl = "~/img/Star.gif";
+            val1--;
+        }
+    }
+    protected void ImageButton1_Click1(object sender, ImageClickEventArgs e)
+    {
+        Label1.Enabled = true;
+        if (val2 == 0)
+        {
+            ImageButton1.ImageUrl = "~/img/FilledStar.gif";
+            ImageButton2.Enabled = true;
+            img1.Enabled = false;
+            val2++;
+            
+        }
+        else
+        {
+            ImageButton1.ImageUrl = "~/img/Star.gif";
+            img1.Enabled = true;
+            val2--;
+            
+        }
+    }
+    protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+    {
+        
+        if (val3 == 0)
+        {
+            ImageButton2.ImageUrl = "~/img/FilledStar.gif";
+            ImageButton3.Enabled = true;
+            img1.Enabled = false;
+            ImageButton1.Enabled = false;
+            val3++;
+           
+        }
+        else
+        {
+            ImageButton2.ImageUrl = "~/img/Star.gif";
+            ImageButton1.Enabled = true;
+            val3--;
+           
+        }
+    }
+   
+    protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+    {
+        
+        if (val4 == 0)
+        {
+            ImageButton3.ImageUrl = "~/img/FilledStar.gif";
+            ImageButton4.Enabled = true;
+            img1.Enabled = false;
+            ImageButton1.Enabled = false;
+            ImageButton2.Enabled = false;
+            val4++;
+           
+        }
+        else
+        {
+            ImageButton3.ImageUrl = "~/img/Star.gif";
+            ImageButton2.Enabled = true;
+            val4--;
+            
+        }
+    }
+
+    protected void ImageButton4_Click(object sender, ImageClickEventArgs e)
+    {
+        
+        if (val5 == 0)
+        {
+            ImageButton4.ImageUrl = "~/img/FilledStar.gif";
+            img1.Enabled = false;
+            ImageButton1.Enabled = false;
+            ImageButton2.Enabled = false;
+            ImageButton3.Enabled = false;
+            val5++;
+           
+        }
+        else
+        {
+            ImageButton4.ImageUrl = "~/img/Star.gif";
+            ImageButton3.Enabled = true;
+            val5--;
+           
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (val1 == 1)
+        {
+            Label1.Text = "Thank You !!";
+        }
+       
+    }
+}
